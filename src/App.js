8f996193b262cf2620a1657a6def5e971a17b266
Rayhaneh <r.b.assady@gmail.com>
2017-09-29 16:38:56 -0400

    Installed font-awesome. Added styling. Added delete button.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import 'font-awesome/css/font-awesome.min.css';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state =  {
+      todos: [],
+      newTodo: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick  = this.handleClick.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({todos: [...this.state.todos, this.state.newTodo], newTodo: ''})
+  }
+
+  handleChange(event) {
+    this.setState({newTodo: event.target.value})
+  }
+
+  handleClick(index) {
+    const todos = [...this.state.todos]
+    todos.splice(index, 1)
+    this.setState({todos: todos})
+  }
+
   render() {
+    const todos = this.state.todos.map((todo, index) => (
+      <li key={index}>
+        <button className="delete-button" onClick={() => this.handleClick(index)}>
+          <i className="fa fa-trash-o fa-2x" aria-hidden="true"></i>
+        </button>
+        <span>{todo}{index}</span>
+      </li>
+    ))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2 className={'App-header'}>Simple todo app</h2>
+        <form className={'todo-form'} onSubmit={this.handleSubmit}>
+          <input
+            name="todo"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+            autoComplete={'off'}
+          />
+          <button type="submit">SAVE</button>
+        </form>
+        <div className="todos-list">
+          <ul>
+            {todos}
+          </ul>
+        </div>
       </div>
     );
   }
